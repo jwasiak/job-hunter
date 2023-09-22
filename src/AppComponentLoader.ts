a50eb602e9b512d100f8cb1b0f905bd90cd097e1
Jarosław Wasiak <jaroslaw@wasiak.biz>
2023-09-22 13:15:48 +0200

    fix: remove unused file

--- src/AppComponentLoader.ts
+++ src/AppComponentLoader.ts
@@ -7,14 +7,12 @@ export const Components = {
   ShowLink: AppComponentLoader.add('ShowLink', './components/show-link'),
   ListLink: AppComponentLoader.add('ListLink', './components/list-link'),
   ShowMailTo: AppComponentLoader.add('ShowMailTo', './components/show-mailto'),
-  // RelationsButton: AppComponentLoader.add('RelationsButton', './components/relations-button'),
   SidebarResourceSection: AppComponentLoader.override(
     'SidebarResourceSection',
     './components/sidebar-resource-section'
   ),
   SidebarBranding: AppComponentLoader.override('SidebarBranding', './components/sidebar-branding'),
   HelpPage: AppComponentLoader.add('HelpPage', './components/help-page'),
-  JmwComponent: AppComponentLoader.add('JmwComponent', './components/jmw-component'),
   ListCounter: AppComponentLoader.add('ListCounter', './components/list-counter'),
   CustomActionButton: AppComponentLoader.add('CustomActionButton', './components/action-button'),
   Invoices: AppComponentLoader.add('Invoices', './components/invoices'),
