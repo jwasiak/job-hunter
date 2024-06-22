a1ef69b8eb8d58dd23f16fc7ac32cf0639bc51dd
Jarosław Wasiak <dev@wasiak.biz>
2024-06-22 18:04:22 +0200

    modify show contact layout

--- src/resources/Contacts/layout.ts
+++ src/resources/Contacts/layout.ts
@@ -33,6 +33,38 @@ const base: Array<LayoutElement> = [
   ],
 ]
 
+const show: Array<LayoutElement> = [
+  [
+    { flex: true, flexDirection: 'row', justifyContent: 'space-between' },
+    [['company', { flexGrow: 1, marginRight: '10px' }], 'statusCode'],
+  ],
+  [
+    { flexDirection: 'row', flex: true },
+    [
+      ['phone', { marginRight: '10px' }],
+      ['email', { flexGrow: 1, marginRight: '10px' }],
+      ['url', { flexGrow: 1 }],
+    ],
+  ],
+  [
+    { flexDirection: 'row', flex: true },
+    [
+      ['sourceId', { flexGrow: 1, marginRight: '10px' }],
+      ['businessId', { flexGrow: 1 }],
+    ],
+  ],
+  'info',
+  'persons',
+  'activities',
+  [
+    { flexDirection: 'row', flex: true },
+    [
+      ['nextActivityCode', { flexGrow: 1, marginRight: '10px' }],
+      ['nextActivityDate', { flexGrow: 1 }],
+    ],
+  ],
+]
+
 const timestamps: LayoutElement = [
   { flexDirection: 'row', flex: true },
   [
@@ -45,5 +77,5 @@ const timestamps: LayoutElement = [
 export default {
   new: base,
   edit: base,
-  show: [...base, timestamps],
+  show: [...show, timestamps],
 }
